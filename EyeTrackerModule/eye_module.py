e6970a636e58c566d4afad5b36a1c6fa63e2fbb4
Spy <101396350+SpyrosThan@users.noreply.github.com>
2022-07-12 13:40:49 +0300

    Update eye_module.py

--- EyeTrackerModule/eye_module.py
+++ EyeTrackerModule/eye_module.py
@@ -1,24 +1,25 @@
+#Eye Tracker Module - Coded by Spy (Github) - Code is completely open source, you can change it and even use it however you want#
 from pickle import TRUE
 import cv2
 import numpy as np
 
 
 def eyeTrack(blue,green,red):
-    cap = cv2.VideoCapture(0)
+    capture = cv2.VideoCapture(0)
     eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
     while True:
-        ret, frame=cap.read()
+        ret, img=capture.read()
     
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
         eyes = eye_cascade.detectMultiScale(gray, 1.2, 4)
         for(x,y,width,height) in eyes:
-            cv2.rectangle(frame,(x,y),(x+width, y+height), (blue,green,red),5)
-        cv2.imshow('frame', frame)
+            cv2.rectangle(img,(x,y),(x+width, y+height), (blue,green,red),5)
+        cv2.imshow('frame', img)
     
     
         if cv2.waitKey(1) == ord('s'):
             break
 
-    cap.release()
+    capture.release()
     cv2.destroyAllWindows()
